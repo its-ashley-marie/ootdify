3f13cd2c3cb1aa73eda71215444ac573d2e60b04
Ashley <AEvangelista@radtorque.com>
2026-08-03 00:44:28 -0700

    added loading, login, sign-in, profile, and add post page; rough draft

--- script.js
+++ script.js
@@ -1,27 +1,159 @@
-let username = "ashley";
-let heartCount = 0;
-let heartCountDisplay = document.getElementById("heartCount");
-let postButton = document.getElementById("postButton");
-let outfitName = "leather jacket fit";
+// 1) ALL VARIABLES
+let signInButton = document.getElementById("signInButton");
 
-console.log(`Outfit: ${outfitName}`);
+let loginButton = document.getElementById("loginButton"); 
 
-function sayHello() {
-    console.log(`Hello, ${username}!`);
+let username = ""
+let password = ""
+
+let postButton = document.getElementById("addPostButton");
+let profileGrid = document.getElementById("profileGrid");
+let posts = [];
+
+let submitPostButton = document.getElementById("submitPostButton");
+let imageInput = document.getElementById("imageInput");
+let imagePreview = document.getElementById("imagePreview");
+let selectedImage = null;
+
+let feedContainer = document.getElementById("feed");
+
+// 2) ALL FUNCTIONS
+function showScreen(screenId) {
+    let screens = document.getElementsByClassName("screen");
+
+    // hide all of them first
+    for (let i = 0; i < screens.length; i++) {
+        screens[i].classList.remove("active");
+    }
+
+    if (screenId === "signIn" ) {
+        signIn();
+    }
+
+    if (screenId === "profile") {
+        renderProfile();
+    }
+
+    if (screenId === "newPost") {
+        resetPostForm();
+    }
+
+    document.getElementById(screenId).classList.add("active");
 }
 
-function incrementHearts() {
-    heartCount++;
-    heartCountDisplay.textContent = `Hearts: ${heartCount}`;
+function signIn() {
+    username = document.getElementById("usernameInput").value;
+    password = document.getElementById("passwordInput").value;
+    saveData();
+}
 
-    if (heartCount >= 10) {
-        console.log("Popular fit!");
-    } else {
-        console.log("New fit!");
+function resetPostForm() {
+    selectedImage = null;
+    imageInput.value = "";
+    imagePreview.src = "";
+    imagePreview.style.display = "none";
+    document.getElementById("newCaptionInput").value = "";
+}
+
+function createPost() {
+    let newCaption = document.getElementById("newCaptionInput").value;
+
+    let post = {
+        caption: newCaption,
+        hearts: 0,
+        img: selectedImage
+    };
+    posts.push(post);
+    saveData();
+}
+
+function renderProfile() {
+    profileGrid.innerHTML = "";
+
+    for (let i = 0; i < posts.length; i++) {
+        let post = posts[i];
+        
+        let postImage = document.createElement("img");
+        postImage.src = post.img;
+        postImage.alt = post.caption;
+
+        // heartButton.textContent = `❤️ ${post.hearts}`;
+        // heartButton.addEventListener("click", function() {
+        //     incrementHeart(i);
+        //     heartButton.textContent = `❤️ ${post.hearts}`;
+        // });
+
+        profileGrid.appendChild(postImage);
     }
 }
 
-postButton.addEventListener("click", function() {
-    incrementHearts();
+function incrementHeart(postIndex) {
+    posts[postIndex].hearts++;
+    console.log(`Post ${postIndex} has ${posts[postIndex].hearts} hearts.`);
+}
+
+function saveData() {
+    let data = {
+        username: username,
+        posts: posts
+    };
+
+    localStorage.setItem("ootidifyData", JSON.stringify(data));
+}
+
+function loadData() {
+    let saved = localStorage.getItem("ootidifyData");
+    if (saved) {
+        let data = JSON.parse(saved);
+        username = data.username;
+        posts = data.posts;
+        return true;
+    }
+    return false;
+}
+// 3) INITIALIZE
+showScreen("loading");
+
+setTimeout(function() {
+    if (loadData()) {
+        showScreen("profile");
+    } else {
+        showScreen("login");
+    }
+}, 3000);
+
+signInButton.addEventListener("click", function() {
+    showScreen("signIn");
 });
+
+loginButton.addEventListener("click", function() {
+    showScreen("profile")
+})
+
+postButton.addEventListener("click", function() {
+    showScreen("newPost");
+});
+
+imageInput.addEventListener("change", function(event) {
+    let img = imageInput.files[0];
+    let reader = new FileReader();
+
+    reader.onload = function(e) {
+        selectedImage = reader.result;
+
+        let imagePreview = document.getElementById("imagePreview");
+        imagePreview.src = selectedImage;
+        imagePreview.style.display = "block";
+    };
+    reader.readAsDataURL(img);
+});
+
+submitPostButton.addEventListener("click", function() {
+    createPost();
+    resetPostForm();
+    showScreen("profile");    
+});
+
+
+
 
